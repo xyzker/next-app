36636ad83cc359fe15fab9eca188a4b6412bc88e
xyzker <410317669@qq.com>
2026-01-03 21:06:48 +0800

    add: create new pages for About, Analytics, and Users; add Hello component

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,14 @@
+const Users = () => {
+    return (
+        <div>
+            <h1>Dashboard Users</h1>
+            <ul className="mt-10">
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+                <li>User 4</li>
+            </ul>
+        </div>
+    )
+}
+export default Users
